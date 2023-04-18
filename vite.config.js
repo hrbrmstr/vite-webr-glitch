d97bf9941fc713431853cdd41ae8bf4452e406d8
Glitch (glitch-hello-react) <none>
2023-04-18 07:59:31 +0000

    🕵🍞 Checkpoint ./vite.config.js:637429/85 ./package.json:637429/1036 ./.glitch-assets:637429/43 ./index.html:637429/17 ./public/styles.css:637429/616

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import rollup from "rollup"
 
 // https://vitejs.dev/config/
 export default defineConfig({
